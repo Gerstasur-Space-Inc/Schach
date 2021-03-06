9e5fc856580a67d1be9528b8a137be28225529a2
Gitea <gitea@fake.local>
2021-03-06 21:14:49 +0100

    -

---
+++ Schach_LTS/Assets/Scripts/Tweeners/IObjectTweener.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface IObjectTweener {
+   
+        void MoveTo(Transform transform, Vector3 targetPosition);
+    
+}
---
+++ Schach_LTS/Assets/Scripts/Tweeners/InstantTweener.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantTweener : MonoBehaviour, IObjectTweener {
+
+    public void MoveTo(Transform transform, Vector3 targetPosition) {
+        transform.position = targetPosition;
+    }
+}
