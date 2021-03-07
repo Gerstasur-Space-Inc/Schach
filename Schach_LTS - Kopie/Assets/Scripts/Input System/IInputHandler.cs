d63fc58d1d95bcc9fadcb5ada530d55531246510
SpacexerDragon5 <aquila.alba@posteo.de>
2021-03-07 13:30:00 +0100

    ende speichertest

--- Schach_LTS - Kopie/Assets/Scripts/Input System/IInputHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IInputHandler {
-    void ProcessInput(Vector3 inputPosition, GameObject selectedObject, Action onClick);
-}
