70ff8030772d753f7d2b63309b2e378ef99cd056
SpacexerDragon5 <aquila.alba@posteo.de>
2021-03-08 09:59:51 +0100

    kamera hinzugefügt

---
+++ Schach_LTS/Assets/Scripts/Kamera/Kamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float RotateX = 5.0f;
+    private float RotateY=5.0f;
+    private float RotateZ= 5.0f;
+
+    public float XRotateSpeed;
+    public float YRotateSpeed;
+    public float ZRotateSpeed;
+
+
+    public int zoom;
+    private Vector3 mousePos;
+   
+
+    void Start()
+    {
+        Debug.Log("Mouse Pos: " + mousePos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateX = Input.GetAxis("Horizontal");
+        RotateY = Input.GetAxis("Vertical");
+        RotateZ = Input.mouseScrollDelta.y;
+       
+
+       
+            rotateCam();//kamera entsprechend drehen
+       
+
+       
+       
+    }
+   
+    void rotateCam() {
+
+        transform.Rotate(Vector3.up, RotateX * Time.deltaTime * XRotateSpeed * XRotateSpeed);//quadratisch: bessere Nutzerinteraktion
+        transform.Rotate(Vector3.right, RotateY * Time.deltaTime * YRotateSpeed * YRotateSpeed);
+        transform.Rotate(Vector3.forward, RotateZ * Time.deltaTime * ZRotateSpeed * ZRotateSpeed);
+
+
+    }
+}
