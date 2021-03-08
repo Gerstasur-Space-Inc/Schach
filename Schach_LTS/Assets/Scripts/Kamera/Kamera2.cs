adf9f51bada6fa3632aa87eef6aed6d7ff0397d7
Leafar2-0 <rafag2@icloud.com>
2021-03-08 22:04:38 +0100

    -

--- Schach_LTS/Assets/Scripts/Kamera/Kamera2.cs
+++ Schach_LTS/Assets/Scripts/Kamera/Kamera2.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Kamera2 : MonoBehaviour
 {
-    public float XRotateSpeed;
-    public float YRotateSpeed;
+    private float RotateSpeed = 200;
 
     void Start()
     {
@@ -15,26 +14,30 @@ public class Kamera2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotateCam(); //kamera entsprechend drehen       
+        rotateCam(); //kamera entsprechend drehen  
     }
 
     void rotateCam()
     {
-        if (Input.GetMouseButtonDown(2) && Input.GetAxis("Mouse X") < 0)
+        if (Input.GetMouseButton(2) && Input.GetAxis("Mouse X") < 0)
         {
-            transform.Rotate(Vector3.left * Time.deltaTime * XRotateSpeed * XRotateSpeed);
+            transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed * -1);
+            // Debug.Log("Es funktioniert wahrscheinlich immer noch nicht");           
         }
-        else if (Input.GetMouseButtonDown(2) && Input.GetAxis("Mouse X") > 0)
+        else if (Input.GetMouseButton(2) && Input.GetAxis("Mouse X") > 0)
         {
-            transform.Rotate(Vector3.right * Time.deltaTime * YRotateSpeed * YRotateSpeed);
+            transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed);
+            // Debug.Log("Es funktioniert wahrscheinlich immer noch nicht");
         }
-        else if (Input.GetMouseButtonDown(2) && Input.GetAxis("Mouse Y") < 0)
+        else if (Input.GetMouseButton(2) && Input.GetAxis("Mouse Y") < 0)
         {
-            transform.Rotate(Vector3.down * Time.deltaTime * XRotateSpeed * XRotateSpeed);
+            transform.Rotate(Vector3.left * Time.deltaTime * RotateSpeed * -1);
+            // Debug.Log("Es funktioniert wahrscheinlich immer noch nicht");
         }
-        else if (Input.GetMouseButtonDown(2) && Input.GetAxis("Mouse Y") > 0)
+        else if (Input.GetMouseButton(2) && Input.GetAxis("Mouse Y") > 0)   
         {
-            transform.Rotate(Vector3.up * Time.deltaTime * YRotateSpeed * XRotateSpeed);
+            transform.Rotate(Vector3.left * Time.deltaTime * RotateSpeed);
+            // Debug.Log("Es funktioniert wahrscheinlich immer noch nicht");
         }
 
     }
