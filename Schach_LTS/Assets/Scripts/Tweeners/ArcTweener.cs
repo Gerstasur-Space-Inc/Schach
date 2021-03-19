600c5a88b9005aec2c4ddca738c2f53f114c0f36
SpacexerDragon5 <aquila.alba@posteo.de>
2021-03-19 21:13:34 +0100

    rafaels figuren
    rafaels figuren wurden hinzugefügt, rafaels schachbrett..., fehlen noch die texturen der schachfiguren
    
    Co-Authored-By: Leafar2-0 <80168503+Leafar2-0@users.noreply.github.com>

--- Schach_LTS/Assets/Scripts/Tweeners/ArcTweener.cs
+++ Schach_LTS/Assets/Scripts/Tweeners/ArcTweener.cs
@@ -2,9 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ArcTweener : MonoBehaviour, IObjectTweener {
-    public void MoveTo(Transform transform, Vector3 targetPosition) {
-        throw new System.NotImplementedException();
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpHeight;
+
+    public void MoveTo(Transform transform, Vector3 targetPosition)
+    {
+        float distance = Vector3.Distance(targetPosition, transform.position);
+        transform.DOJump(targetPosition, jumpHeight, 1, distance / movementSpeed);
     }
 }
--- Schach_LTS/Assets/Scripts/Tweeners/LineTweener.cs
+++ Schach_LTS/Assets/Scripts/Tweeners/LineTweener.cs
@@ -1,10 +1,13 @@
 ﻿
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LineTweener : MonoBehaviour, IObjectTweener {
+    [SerializeField] private float movementSpeed;
     public void MoveTo(Transform transform, Vector3 targetPosition) {
-        throw new System.NotImplementedException();
+        float distance = Vector3.Distance(targetPosition, transform.position);
+        transform.DOMove(targetPosition, distance / movementSpeed);
     }
 }
