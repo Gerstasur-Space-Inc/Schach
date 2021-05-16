d3d3bb9a6245e51aa2510cc12dac1983a27e3a4d
SpacexerDragon5 <68651941+SpacexerDragon5@users.noreply.github.com>
2021-05-16 08:27:31 +0200

    camera update 1

--- Schach_LTS/Assets/Scripts/mp/CameraSetup.cs
+++ Schach_LTS/Assets/Scripts/mp/CameraSetup.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class CameraSetup : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
+    [SerializeField] private Camera viewCam;
+    bool isviewCam = false;
 
     public void SetupCamera(TeamColor team)
     {
@@ -19,5 +21,39 @@ public class CameraSetup : MonoBehaviour
     {
         mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, -mainCamera.transform.position.z);
         mainCamera.transform.Rotate(Vector3.up, 180f, Space.World);
+    }
+    public void FlipCamerawithDelay()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        changeCam();
+    }
+
+    private void changeCam()
+    {
+        if (Input.GetKeyDown(KeyCode.V)) { isviewCam = isviewCam ? false : true; }
+        if (isviewCam)
+        {
+            switchToViewCam();
+        }
+        else
+        {
+            switchToPlayCam();
+        }
+
+    }
+
+    public void switchToViewCam()
+    {
+        viewCam.enabled = true;
+        Camera.main.enabled = false;
+    }
+    public void switchToPlayCam()
+    {
+        viewCam.enabled = false;
+        Camera.main.enabled = true;
     }
 }
